2459c0c77cd895ed338b0c6c594d7d1ae21b8c6d
fivunlm <lopez.fernando.damian@gmail.com>
2017-06-29 06:59:45 -0300

    #VIN.4.3 Added notification demo page

--- src/demo/app/notification/notification-demo.component.ts
+++ src/demo/app/notification/notification-demo.component.ts
@@ -1,8 +1,24 @@
 import {Component} from "@angular/core";
+import {VnkNotificationPayload, VnkNotificationService, VnkNotificationType} from "vinkel";
+
 
 @Component({
   templateUrl: './notification-demo.component.html'
 })
 export class NotificationDemoComponent {
+  constructor(private _service: VnkNotificationService){}
 
+  onShowBasicNotification(){
+    this._service.open('This is a notification!', 'Hey!');
+  }
+
+  onShowError(){
+    let payload: VnkNotificationPayload = {
+      title: 'Error!',
+      text: 'This is an error example',
+      duration: 3000,
+      type: VnkNotificationType.Danger
+    }
+    this._service.open(payload);
+  }
 }
